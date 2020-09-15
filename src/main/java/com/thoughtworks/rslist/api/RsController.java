fbf0e0e24fd0282ef29b3c65edb49cb281652853
LiYuan1229 <823260355@qq.com>
2020-09-15 22:49:01 +0800

    [liyuan]:finish add only event name

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,19 +1,25 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
 
-  private List<String> rsList = Arrays.asList("第一条事件","第二条事件","第三条事件");
+  private List<String> rsList = initRsList();
 
-  @GetMapping("/rs/list")
+  private List<String> initRsList(){
+      ArrayList<String> tempList = new ArrayList<>();
+      tempList.add("第一条事件");
+      tempList.add("第二条事件");
+      tempList.add("第三条事件");
+      return tempList;
+  }
+
+    @GetMapping("/rs/list")
   public String geAllEvent(@RequestParam(required = false) Integer start,
                               @RequestParam(required = false) Integer end
                             ){
@@ -23,10 +29,14 @@ public class RsController {
       return rsList.subList(start-1,end).toString();
   }
 
-  @GetMapping("rs/{index}")
+  @GetMapping("/rs/{index}")
   public String getOneEvent(@PathVariable int index){
     return rsList.get(index-1);
   }
 
+  @PostMapping("/rs/event")
+  public void addEventHaveEventNameAndKeyword(@RequestBody String addString){
+      rsList.add(addString);
+  }
 
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
@@ -12,10 +14,12 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class RsListApplicationTests {
     @Autowired
     MockMvc mockMvc;
@@ -51,6 +55,29 @@ class RsListApplicationTests {
         int status = response.getStatus();
         assertEquals(200,status);
         assertEquals("[第一条事件, 第二条事件, 第三条事件]",response.getContentAsString());
+    }
+
+    @Test
+    void add_event_have_eventName_and_keyword() throws Exception {
+        MvcResult mvcResult1 = mockMvc.perform(get("/rs/list"))
+                .andReturn();
+        MockHttpServletResponse response1 = mvcResult1.getResponse();
+        int status1 = response1.getStatus();
+        assertEquals(200,status1);
+        assertEquals("[第一条事件, 第二条事件, 第三条事件]",response1.getContentAsString());
+
+        MvcResult mvcResult2 = mockMvc.perform(post("/rs/event").content("第四条事件"))
+                .andReturn();
+        MockHttpServletResponse response2 = mvcResult2.getResponse();
+        int status2 = response2.getStatus();
+        assertEquals(200,status2);
+
+        MvcResult mvcResult3 = mockMvc.perform(get("/rs/list"))
+                .andReturn();
+        MockHttpServletResponse response3 = mvcResult3.getResponse();
+        int status3 = response3.getStatus();
+        assertEquals(200,status3);
+        assertEquals("[第一条事件, 第二条事件, 第三条事件, 第四条事件]",response3.getContentAsString());
     }
 
 
