bb007a453f6a1fa5c2cc7224a45bf7e9422a52f3
LiYuan1229 <823260355@qq.com>
2020-09-17 19:48:54 +0800

    [liyuan]:finish user save to mysql

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -2,6 +2,9 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.dto.RsEvent;
 import com.thoughtworks.rslist.dto.UserInfo;
+import com.thoughtworks.rslist.entity.UserEntity;
+import com.thoughtworks.rslist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,21 +14,32 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    public List<UserInfo> userList = initUserList();
-    public List<UserInfo> initUserList() {
-        ArrayList<UserInfo> tempList = new ArrayList<>();
-        tempList.add(new UserInfo("xiaowang1",19,"female","a@thoughtworks.com","18888888888"));
-        return tempList;
-    }
+//    public List<UserInfo> userList = initUserList();
+    @Autowired
+    UserRepository userRepository;
+//    public List<UserInfo> initUserList() {
+//        ArrayList<UserInfo> tempList = new ArrayList<>();
+//        tempList.add(new UserInfo("xiaowang1",19,"female","a@thoughtworks.com","18888888888"));
+//        return tempList;
+//    }
 
-    @GetMapping("/user/list")
-    public ResponseEntity<List<UserInfo>> geAllUser(){
-        return  ResponseEntity.ok().body(userList);
-    }
+//    @GetMapping("/user/list")
+//    public ResponseEntity<List<UserInfo>> geAllUser(){
+//        return  ResponseEntity.ok().body(userList);
+//    }
 
     @PostMapping("/user/register")
     public void register(@Valid @RequestBody UserInfo userInfo){
-          userList.add(userInfo);
+        UserEntity userEntity=UserEntity.builder()
+                .userName(userInfo.getUserName())
+                .email(userInfo.getEmail())
+                .age(userInfo.getAge())
+                .gender(userInfo.getGender())
+                .phone(userInfo.getPhone())
+                .voteNum(userInfo.getAge())
+                .build();
+        userRepository.save(userEntity);
+
     }
 
 
